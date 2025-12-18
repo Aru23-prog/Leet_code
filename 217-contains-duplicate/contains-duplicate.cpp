6f8b990001a96b4dfc0c4dbdee630e7cc10516d1
Aru23-prog <msarunima2009@gmail.com>
2025-12-18 22:27:09 +0530

    Time: 51 ms (56.41%) | Memory: 91 MB (29.90%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> c;
+        for(int i=0;i<nums.size();i++){
+            c[nums[i]]++;
+            if(c[nums[i]]>1)return true;
+        }
+        return false;
+    }
+};
